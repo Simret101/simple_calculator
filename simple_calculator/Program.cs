4c8f19444298b3356e008034713a73c23b62420c
USER <USER@DESKTOP-IK076IC>
2024-06-29 23:56:25 +0300

    Add project files.

---
+++ simple_calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            String ans;
+            int result;
+            Console.WriteLine("Welcome, Please Enter the first number.");
+            num1 = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Please Enter the Second number.");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the kind of operation you want to do."); 
+            Console.WriteLine("        If it is addition enter + symbol, ");
+            Console.WriteLine("        If it is subtraction enter - , ");
+            Console.WriteLine("        If it is  enter * ,");
+            Console.WriteLine("        If it is division enter / .");
+
+            ans = Console.ReadLine();
+            if (ans == "+") 
+            {
+                result = num1 + num2; 
+
+            }
+            else if (ans == "-")
+            { 
+                result = num1 - num2;
+                
+            }
+            else if (ans == "*")
+            { 
+                result = num1 * num2;
+                
+            }
+            else 
+            { 
+                result = num1 / num2;
+
+                
+            }
+            Console.WriteLine($"The Answer is { result}");
+            Console.WriteLine("Thank You for using Our Calculator!");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
